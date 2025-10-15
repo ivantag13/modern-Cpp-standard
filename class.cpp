0fbb10588ef219ae3b687e03793db6216e088a87
Ivan Tagliaferro <ivantagliaferroot@gmail.com>
2025-10-15 14:32:30 +0200

    Add operator overloading

--- class.cpp
+++ class.cpp
@@ -12,18 +12,46 @@ public:
 
     ~Point() { std::cout << "Point destroyed.\n"; }
 
+    // Accessors
+    float getX() const { return this->x; }
+    float getY() const { return this->y; }
+
+    void setX(float val) { this->x = val; }
+    void setY(float val) { this->y = val; }
+
     // Method
-    void move(float dx, float dy)
+    void translate(float dx, float dy)
     {
         x += dx;
         y += dy;
+    }
+
+    float norm(Point p) const
+    {
+        return std::sqrt(p.getX() * p.getX() + p.getY() * p.getY());
+    }
+
+    Point operator+(const Point &other) const
+    {
+        return Point(this->x + other.getX(), this->y + other.getY());
+    }
+
+    Point operator-(const Point &other) const
+    {
+        return Point(this->x - other.getX(), this->y - other.getY());
+    }
+
+    bool operator==(const Point &other) const
+    {
+        return x == other.getX() && y == other.getY();
     }
 
     float distance_to(const Point &other) const
     {
         float dx = x - other.x;
         float dy = y - other.y;
-        return std::sqrt(dx * dx + dy * dy);
+        Point p(dx, dy);
+        return norm(p);
     }
 
     // Method to print
@@ -39,11 +67,11 @@ private:
     Point p1, p2;
 
 public:
-    LineSegment(Point p1_val, Point p2_val) : p1(p1_val), p2(p2_val) {}
+    LineSegment(const Point &p1_val, const Point &p2_val) : p1(p1_val), p2(p2_val) {}
 
-    ~LineSegment() { std::cout << "Line segment destroyed"; }
+    ~LineSegment() { std::cout << "Line segment destroyed\n"; }
 
-    float length()
+    float length() const
     {
         return p1.distance_to(p2);
     }
@@ -62,12 +90,22 @@ public:
 int main()
 {
     Point p(2.0f, 3.0f); // calls constructor
-    p.move(1.0f, -2.0f);
+    p.translate(1.0f, -2.0f);
     p.print(); // prints (3, 1)
 
     Point *p_p = new Point(1, 2); // allocated on heap
-    p_p->move(4.9, 0.3);
+    p_p->translate(4.9, 0.3);
     p_p->print();
+
+    Point c = p + *p_p;
+    std::cout << "Print point c: ";
+    c.print();
+
+    Point d = p - *p_p;
+    std::cout << "Print point d: ";
+    d.print();
+
+    std::cout << "Is c equal to d: " << (d == c) << "\n";
 
     float distance_p_p_to_p = p_p->distance_to(p);
     float distance_p_to_p_p = p.distance_to(*p_p);
@@ -83,5 +121,5 @@ int main()
     float area = r.area();
     std::cout << "Area: " << area << " \n";
 
-    return 0;
+    return 0; // RAII (Resource Acquisition Is Initialization) takes care of freeing any used memory
 }
